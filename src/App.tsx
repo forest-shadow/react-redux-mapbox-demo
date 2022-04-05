24212cf5730045ccd4b16633dc897b58bb9796fc
forest-shadow <ivan.arevkov@gmail.com>
2022-04-05 17:23:12 +0300

    feat: added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Map from 'react-map-gl';
 
-function App() {
+import './index.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const APP_BAR_HEIGHT = 64;
+
+const App = () => {
+  console.log(process.env)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            VertexTest
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14
+        }}
+        style={{width: '100%', height: window.innerHeight - APP_BAR_HEIGHT}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      />
+    </Box>
   );
 }
 
