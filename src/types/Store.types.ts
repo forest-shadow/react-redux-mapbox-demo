948f4c6432a8c66e48af5e27bf8a46579daa93b8
forest-shadow <ivan.arevkov@gmail.com>
2023-03-26 21:19:54 +0200

    chore(Store.types) got rid of out-of-use TThunkActionCreator type

--- src/types/Store.types.ts
+++ src/types/Store.types.ts
@@ -1,5 +1,4 @@
-import {ActionCreator, AnyAction, ThunkAction, ThunkDispatch} from "@reduxjs/toolkit";
+import {AnyAction, ThunkDispatch} from "@reduxjs/toolkit";
 import {IAppState} from "../store";
 
-export type TThunkActionCreator = ActionCreator<ThunkAction<void, IAppState, unknown, AnyAction>>;
 export type TThunkDispatch = ThunkDispatch<IAppState, unknown, AnyAction>;
